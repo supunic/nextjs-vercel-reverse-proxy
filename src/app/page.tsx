ca61cc9bd1765af73a7e04587e354db5d8a3f220
supunic <seiya.endo0830@gmail.com>
2024-10-07 20:57:37 +0900

    ボタン追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Link href="/google">google</Link>
       <main className={styles.main}>
         <Image
           className={styles.logo}
